68cd7d475e451e37841d4b414e5aa6290b54751a
shashank1601 <shashank1601@gmail.com>
2015-04-19 17:51:07 -0400

    small changes made

--- features/step_definitions/ForgotPassword_steps.rb
+++ features/step_definitions/ForgotPassword_steps.rb
@@ -1,10 +1,10 @@
 Given(/^I'm on the Forgot Password page$/) do
-  visit(forgot_password_path)
+  visit(forgot_password_index_path)
 end
 
 When(/^I fill in the form and submit the correct email of the user$/) do
-	fill_in 'login_email', :with => "sibi@gmail.com"
-	click_button 'Get Password'
+	fill_in 'forgot_password_validate_email_id', :with => "sibivkumar@gmail.com"
+	click_button 'Get Information'
 end
 
 Then(/^I should go back to the Login Page$/) do
@@ -12,19 +12,19 @@ Then(/^I should go back to the Login Page$/) do
 end
 
 When(/^I fill in the form and submit empty email of the user$/) do
-	fill_in 'login_email', :with => ""
-	click_button 'Get Password'
+	fill_in 'forgot_password_validate_email_id', :with => ""
+	click_button 'Get Information'
 end
 
 Then(/^forgot password page should have notice message$/) do
-   assert page.has_content?('Your email field was empty.')
+   assert page.has_content?('Field cannot be blank')
 end
 
 When(/^I fill in the form and submit wrong email of the user$/) do
-	fill_in 'login_email', :with => "0sibi@gmail.com"
-	click_button 'Get Password'
+	fill_in 'forgot_password_validate_email_id', :with => "0sibi@gmail.com"
+	click_button 'Get Information'
 end
 
 Then(/^forgot password page should display notice message$/) do
-   assert page.has_content?('Your email was incorrect.')
+   assert page.has_content?('Email Address does not exist')
 end
