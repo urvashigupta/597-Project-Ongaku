539b1295ae81858fc0a04902db49dbf565934271
SIBI V <sibivkumar@gmail.com>
2015-04-20 02:44:45 -0400

    Added homepage feature

--- features/homepage.feature
+++ features/homepage.feature
@@ -1,6 +1,11 @@
 Feature: Homepage
 	Shows the homepage of a registered user
 
+	Scenario: Reset search field
+		Given I'm on the User homepage
+		When I click clear
+		Then I should see the a cleared search field
+
 	Scenario: Logged out
 		Given I'm on the User homepage
 		When I click logout
--- features/step_definitions/homepage_steps.rb
+++ features/step_definitions/homepage_steps.rb
@@ -1,5 +1,6 @@
 Given(/^I'm on the User homepage$/) do
   visit home_index_path
+  fill_in 'searching', :with => "sibivkumar"
 end
 
 When(/^I click logout$/) do
@@ -8,4 +9,12 @@ end
 
 Then(/^I should go back to Login page$/) do
   assert page.has_content?('You have been logged out')
+end
+
+When(/^I click clear$/) do
+  click_button 'Clear'
+end
+
+Then(/^I should see the a cleared search field$/) do
+  assert page.has_field?('searching', :with => '') 
 end
