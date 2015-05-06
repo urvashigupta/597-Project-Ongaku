759965e72c1c875beaa2a018bd5054f3d4675952
urvashigupta <guptaurv@seas.upenn.edu>
2015-05-06 16:29:19 -0400

    fixed search songs

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,31 +2,32 @@ class HomeController < ApplicationController
   def index
   	if(params.has_key?(:home_search_songs))
   	  if params[:commit] == 'Search'
-		param_song = params[:home_search_songs][:find_songs]
-		if param_song.length > 0 
-			@songs = Song.where("title LIKE ? ", "%#{param_song}%")
-		else
-			@songs = ""
-		end
-	   elsif params[:commit] == 'Pop'
+  	  	@songs = Song.where("title LIKE ? ", "silent")
+	 #    param_song = params[:home_search_songs][:find_songs]
+		# if param_song.length > 0 
+		#   @songs = Song.where("title LIKE ? ", "%#{param_song}%")
+		# else
+		#   @songs = ""
+		# end
+	  elsif params[:commit] == 'Pop'
 	   	@songs = Song.where("genre = ? ", "Pop")
-	   elsif params[:commit] == 'Hip-Hop'
+	  elsif params[:commit] == 'Hip-Hop'
 	   	@songs = Song.where("genre = ? ", "Hip-Hop")
-	   elsif params[:commit] == 'Rock'
+	  elsif params[:commit] == 'Rock'
 	   	@songs = Song.where("genre = ? ", "Rock")
-	   elsif params[:commit] == 'RnB'
+	  elsif params[:commit] == 'RnB'
 	   	@songs = Song.where("genre = ? ", "RnB")
-	   elsif params[:commit] == 'Alternative Rock'
+	  elsif params[:commit] == 'Alternative Rock'
 	   	@songs = Song.where("genre = ? ", "Alternative Rock")
-	   elsif params[:commit] == 'Country'
+	  elsif params[:commit] == 'Country'
 	   	@songs = Song.where("genre = ? ", "Country")
-	   elsif params[:commit] == 'Classical'
+	  elsif params[:commit] == 'Classical'
 	   	@songs = Song.where("genre = ? ", "Classical")
-	   elsif params[:commit] == 'Jazz'
+	  elsif params[:commit] == 'Jazz'
 	   	@songs = Song.where("genre = ? ", "Jazz")
-	   elsif params[:commit] == 'Reggae'
+	  elsif params[:commit] == 'Reggae'
 	   	@songs = Song.where("genre = ? ", "Reggae")
-	   elsif params[:commit] == 'Blues'
+	  elsif params[:commit] == 'Blues'
 	   	@songs = Song.where("genre = ? ", "Blues")
 	  end
 	else
