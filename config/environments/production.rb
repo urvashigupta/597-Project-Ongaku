6a3a281db38e332586428aafccaca1b3cafbd1c1
urvashigupta <guptaurv@seas.upenn.edu>
2015-05-08 01:08:20 -0400

    deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,15 +66,15 @@ MusicApp::Application.configure do
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
 
-  config.action_mailer.delivery_method = :smtp
-  # SMTP settings for mailgun
-  ActionMailer::Base.smtp_settings = {
-    :port           => 587,
-    :address        => "smtp.mailgun.org",
-    :domain         => ENV['domain'],
-    :user_name      => ENV['username'],
-    :password       => ENV['password'],
-    :authentication => :plain,
-  }}
+  # config.action_mailer.delivery_method = :smtp
+  # # SMTP settings for mailgun
+  # ActionMailer::Base.smtp_settings = {
+  #   :port           => 587,
+  #   :address        => "smtp.mailgun.org",
+  #   :domain         => ENV['domain'],
+  #   :user_name      => ENV['username'],
+  #   :password       => ENV['password'],
+  #   :authentication => :plain,
+  # }}
 
 end
